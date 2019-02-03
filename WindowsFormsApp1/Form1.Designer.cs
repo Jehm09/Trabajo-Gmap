b214fc357eb45141e9640e901df3a04fe7978b22
Jehm09 <estephenher999@gmail.com>
2019-02-03 12:19:41 -0500

    Update Windows Form

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -24,17 +24,20 @@
         /// </summary>
         private void InitializeComponent() {
             this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gMap
             // 
             this.gMap.Bearing = 0F;
             this.gMap.CanDragMap = true;
+            this.gMap.Cursor = System.Windows.Forms.Cursors.Default;
             this.gMap.EmptyTileColor = System.Drawing.Color.Navy;
             this.gMap.GrayScaleMode = false;
             this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMap.LevelsKeepInMemmory = 5;
-            this.gMap.Location = new System.Drawing.Point(0, 1);
+            this.gMap.Location = new System.Drawing.Point(12, 44);
             this.gMap.MarkersEnabled = true;
             this.gMap.MaxZoom = 18;
             this.gMap.MinZoom = 2;
@@ -48,26 +51,51 @@
             this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMap.ShowTileGridLines = false;
-            this.gMap.Size = new System.Drawing.Size(798, 448);
+            this.gMap.Size = new System.Drawing.Size(758, 397);
             this.gMap.TabIndex = 1;
-            this.gMap.Zoom = 13D;
+            this.gMap.Zoom = 5D;
             this.gMap.Load += new System.EventHandler(this.gMap_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.gMap);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Accidentes Transito";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl gMap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GMap;
+using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
 
 namespace WindowsFormsApp1 {
     public partial class Form1 : Form {
@@ -17,11 +20,31 @@ namespace WindowsFormsApp1 {
 
         private void gMap_Load(object sender, EventArgs e) {
             // Initialize map:
-            gMap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gMap.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
-            //gMap.Position = new GMap.NET.PointLatLng(48.8589507, 2.2775175);
-            gMap.SetPositionByKeywords("Cali, Colombia");
+            gMap.SetPositionByKeywords("Bogota, Colombia");
+
+            //Cambiando opciones del Gmap, no mostrar el centro y cambiando el boton derecho a izquierdo para mover el mapa.
             gMap.ShowCenter = false;
+            gMap.DragButton = MouseButtons.Left;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //gMap.SetPositionByKeywords(textBox1.Text);
+            GMapOverlay markerOverlay = new GMapOverlay("Marcador");
+            //GMap.NET.PointLatLng p = gMap.SetPositionByKeywords("asd", 464646,646546);
+            //gMap.GetPositionByKeywords("Universidad Icesi, Colombia");
+            //GMarkerGoogle marker = new GMarkerGoogle(gMap.GetPositionByKeywords("Universidad Icesi, Colombia"), GMarkerGoogleType.green);
+            GMarkerGoogle marker = new GMarkerGoogle(new GMap.NET.PointLatLng(-25.971684, 32.589759), GMarkerGoogleType.black_small);
+            markerOverlay.Markers.Add(marker); //agregar marker al mapa
+            gMap.Position = new GMap.NET.PointLatLng(-25.971684, 32.589759);
+
+            gMap.Overlays.Add(markerOverlay);
+        }
+
+        public void addMarkers(String ) {
+
         }
     }
 }
